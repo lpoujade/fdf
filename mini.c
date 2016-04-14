39f8af11170acae9080bf81b2f3ae1f14bc528fd
lpoujade <lpoujade@student.42.fr>
2016-04-14 21:17:55 +0200

    Parsing with t_coord *

--- mini.c
+++ mini.c
@@ -6,19 +6,26 @@
 /*   By: liums <lpoujade@student.42.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/11 19:18:17 by liums             #+#    #+#             */
-/*   Updated: 2016/04/14 20:53:47 by lpoujade         ###   ########.fr       */
+/*   Updated: 2016/04/14 21:09:44 by lpoujade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "libft.h"
 
-int		parse_file(char *file, int ***otab);
+typedef struct		s_coords
+{
+	int				x;
+	int				y;
+	int				z;
+}					t_coords;
+
+int		parse_file(char *file, t_coords **tab);
 
 int	main(int ac, char **av)
 {
 	(void)ac;
-	int **tab = NULL;
+	t_coords	*tab = NULL;
 	int count, c = 0;
 
 	if ((count = parse_file(av[1], &tab)) < 0)
@@ -27,9 +34,9 @@ int	main(int ac, char **av)
 	while (c < count)
 	{
 		//ft_putnbr(count);ft_putstr("\t:");
-		ft_putnbr(tab[0][c]); ft_putchar(' ');
-		ft_putnbr(tab[1][c]); ft_putchar(' ');
-		ft_putnbr(tab[2][c]); ft_putchar('\n');
+		ft_putnbr(tab[c].x); ft_putchar(' ');
+		ft_putnbr(tab[c].y); ft_putchar(' ');
+		ft_putnbr(tab[c].z); ft_putchar('\n');
 		//printf("x: %d| y: %d| z: %d\n", tab[0][c], tab[1][c], tab[2][c]);
 		c++;
 	}
--- src/parse.c
+++ src/parse.c
@@ -6,7 +6,7 @@
 /*   By: lpoujade <lpoujade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/15 14:46:42 by lpoujade          #+#    #+#             */
-/*   Updated: 2016/04/14 20:53:47 by lpoujade         ###   ########.fr       */
+/*   Updated: 2016/04/14 21:09:30 by lpoujade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,46 +15,45 @@
 #include <fcntl.h>
 #include "libft.h"
 
-static inline void	resize(int **tab, size_t act_size, size_t nsize)
+typedef struct		s_coords
 {
-	int		*new;
-	int		c;
+	int				x;
+	int				y;
+	int				z;
+}					t_coords;
+
+static inline void	resize(t_coords **tab, size_t act_size, size_t nsize)
+{
+	t_coords		*new;
 
 	if (act_size >= nsize)
 		exit (2);
 	ft_putendl_fd("WILL PROBABLY SEGFAULT", 2);
-	c = 2;
-	while (c + 1)
-	{
-		if (!(new = (int*)malloc(nsize * sizeof(int))))
-			exit(12);
-		ft_memcpy(new, (tab + c), act_size);
-		//*(tab + c) = new;
-		tab[c] = new;
-		c--;
-	}
+	if (!(new = malloc(nsize * sizeof(t_coords))))
+		exit(12);
+	ft_memcpy(new, tab, act_size);
+	//tab = new;
+	ft_swap(&tab, &new);
 }
 
 /*
 ** A(x, y, z) = (tab[0][A], tab[1][A], tab[2][A])
 */
 
-int					parse_file(char *file, int ***otab)
+int					parse_file(char *file, t_coords **f)
 {
 	int		fd;
 	char	*line;
-	int		**tab;
+	t_coords *tab;
 	int		x;
 	int		y;
 	int		w;
-	int		ts = 600;
+	int		ts;
 
+	ts = 200;
 	y = 0;
 	w = 0;
-	if (!(tab = (int**)malloc(3 * sizeof(int*)))
-			|| !(tab[0] = (int*)malloc(ts * sizeof(int)))
-			|| !(tab[1] = (int*)malloc(ts * sizeof(int)))
-			|| !(tab[2] = (int*)malloc(ts * sizeof(int)))
+	if (!(tab = malloc(ts * sizeof(t_coords)))
 			|| (fd = open(file, O_RDONLY)) < 0)
 		return (-1);
 	while (get_next_line(fd, &line))
@@ -62,22 +61,23 @@ int					parse_file(char *file, int ***otab)
 		x = 0;
 		while (*line)
 		{
-			tab[0][w] = x;
-			tab[2][w] = ft_atoi(line);
-			tab[1][w] = y;
-			line += ft_getndigits(tab[2][w]);
+			tab[w].x = x;
+			tab[w].y = y;
+			tab[w].z = ft_atoi(line);
+
+			line += ft_getndigits(tab[w].z);
 			*line ? line++ : 0;
 			while (*line && !(ft_isdigit(*line) && *(line - 1) == ' '))
 				line++;
 			x++;
 			if (++w >= ts)
 			{
-				resize(tab, ts, ts * 2);
+				resize(&tab, ts, ts * 2);
 				ts *= 2;
 			}
 		}
 		y++;
 	}
-	*otab = tab;
+	*f = tab;
 	return (y * x);
 }
