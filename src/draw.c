3f49b3e66f772047a6831fc040b0afd35d07b72e
lpoujade <lpoujade@student.42.fr>
2016-05-05 19:01:52 +0200

    wip

--- src/draw.c
+++ src/draw.c
@@ -6,7 +6,7 @@
 /*   By: lpoujade <lpoujade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/15 13:12:30 by lpoujade          #+#    #+#             */
-/*   Updated: 2016/05/05 16:17:41 by lpoujade         ###   ########.fr       */
+/*   Updated: 2016/05/05 19:00:44 by lpoujade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,11 +61,8 @@ int					line(int const *coord, int *dim, t_pixel *first)
 	yp.y = coord[3];
 	if (yp.x == yp.y || xp.x == xp.y)
 		return (vh_lines(coord, dim, first));
-	if ((ft_abs(yp.y - yp.x) > ft_abs(xp.y - xp.x)))
-	{
-		ft_iswap(&xp.x, &yp.x);
-		ft_iswap(&xp.y, &yp.y);
-	}
+	if ((ft_abs(yp.y - yp.x) > ft_abs(xp.y - xp.x)) && 0) // --- 
+		ft_swap(&xp, &yp);
 	if (xp.x > xp.y)
 	{
 		ft_iswap(&xp.x, &xp.y);
@@ -87,20 +84,21 @@ int					line(int const *coord, int *dim, t_pixel *first)
 t_map				tr(t_map orig, int *dims)
 {
 	int				c;
-	double 			ct;
 	int				x;
 
 	c = 0;
-	ct = 0.6;
 	(void)dims;
 	while (c < orig.dims.z)
 	{
-		//orig.pts[c].x += orig.dims.x;
 		x = orig.pts[c].x;
-		orig.pts[c].x = (x + orig.pts[c].y) / 2;
-		orig.pts[c].y = orig.pts[c].z + ((x - orig.pts[c].y) / 1.5);
-		orig.pts[c].x *= 10; orig.pts[c].y *= 10;
+		orig.pts[c].x = (x - orig.pts[c].y) / 2;
+		orig.pts[c].y = orig.pts[c].z + ((x + orig.pts[c].y) / 1.5);
 		/*
+		orig.pts[c].x *= 10;
+		orig.pts[c].y *= 10;
+			*/
+		orig.pts[c].x += orig.dims.x/4;
+		orig.pts[c].y += orig.dims.y/4;
 		if (orig.pts[c].x < 0 || orig.pts[c].y < 0)
 			ft_putendl("NEGATIVE POINTS !");
 		orig.pts[c].x =
@@ -109,7 +107,6 @@ t_map				tr(t_map orig, int *dims)
 			(((10000 * orig.pts[c].y) / orig.dims.y) * (dims[1])) / 10000;
 		if (orig.pts[c].x > dims[0] || orig.pts[c].y > dims[1])
 			ft_putendl("WRONG CALC");
-			*/
 		c++;
 	}
 	ft_putendl("pts -> 3dified");
